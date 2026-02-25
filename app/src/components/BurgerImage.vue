61df2d213a5200862f5a250b8447894d46319c82
Raymondl177 <raymondl177@nycstudents.net>
2026-02-25 12:38:19 -0500

    h

---
+++ app/src/components/BurgerImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img src="burger.png" alt="Your Burger" class="w-full h-auto" />
+
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  showBurger: Function
+})
+</script>
+
+<style scoped>
+
+</style>
